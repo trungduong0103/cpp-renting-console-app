a994bd86b4d2a5f64498ec469e2a81219f2d459c
trungduong0103 <trungduong0103@gmail.com>
2021-05-17 21:55:32 +0700

    updated

--- cmake-build-debug/CMakeFiles/cpp_renting_console_app.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/cpp_renting_console_app.dir/DependInfo.cmake
@@ -4,17 +4,17 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "C:/Users/ryanz/Codes/School/cpp-renting-console-app/main.cpp" "C:/Users/ryanz/Codes/School/cpp-renting-console-app/cmake-build-debug/CMakeFiles/cpp_renting_console_app.dir/main.cpp.obj"
-  "C:/Users/ryanz/Codes/School/cpp-renting-console-app/sources/Customer.cpp" "C:/Users/ryanz/Codes/School/cpp-renting-console-app/cmake-build-debug/CMakeFiles/cpp_renting_console_app.dir/sources/Customer.cpp.obj"
-  "C:/Users/ryanz/Codes/School/cpp-renting-console-app/sources/CustomerHelpers.cpp" "C:/Users/ryanz/Codes/School/cpp-renting-console-app/cmake-build-debug/CMakeFiles/cpp_renting_console_app.dir/sources/CustomerHelpers.cpp.obj"
-  "C:/Users/ryanz/Codes/School/cpp-renting-console-app/sources/CustomerRepository.cpp" "C:/Users/ryanz/Codes/School/cpp-renting-console-app/cmake-build-debug/CMakeFiles/cpp_renting_console_app.dir/sources/CustomerRepository.cpp.obj"
-  "C:/Users/ryanz/Codes/School/cpp-renting-console-app/sources/Item.cpp" "C:/Users/ryanz/Codes/School/cpp-renting-console-app/cmake-build-debug/CMakeFiles/cpp_renting_console_app.dir/sources/Item.cpp.obj"
-  "C:/Users/ryanz/Codes/School/cpp-renting-console-app/sources/ItemHelpers.cpp" "C:/Users/ryanz/Codes/School/cpp-renting-console-app/cmake-build-debug/CMakeFiles/cpp_renting_console_app.dir/sources/ItemHelpers.cpp.obj"
-  "C:/Users/ryanz/Codes/School/cpp-renting-console-app/sources/ItemRepository.cpp" "C:/Users/ryanz/Codes/School/cpp-renting-console-app/cmake-build-debug/CMakeFiles/cpp_renting_console_app.dir/sources/ItemRepository.cpp.obj"
-  "C:/Users/ryanz/Codes/School/cpp-renting-console-app/sources/Menu.cpp" "C:/Users/ryanz/Codes/School/cpp-renting-console-app/cmake-build-debug/CMakeFiles/cpp_renting_console_app.dir/sources/Menu.cpp.obj"
-  "C:/Users/ryanz/Codes/School/cpp-renting-console-app/sources/ServiceBuilder.cpp" "C:/Users/ryanz/Codes/School/cpp-renting-console-app/cmake-build-debug/CMakeFiles/cpp_renting_console_app.dir/sources/ServiceBuilder.cpp.obj"
+  "/Users/trung/CLionProjects/cpp-renting-console-app/main.cpp" "/Users/trung/CLionProjects/cpp-renting-console-app/cmake-build-debug/CMakeFiles/cpp_renting_console_app.dir/main.cpp.o"
+  "/Users/trung/CLionProjects/cpp-renting-console-app/sources/Customer.cpp" "/Users/trung/CLionProjects/cpp-renting-console-app/cmake-build-debug/CMakeFiles/cpp_renting_console_app.dir/sources/Customer.cpp.o"
+  "/Users/trung/CLionProjects/cpp-renting-console-app/sources/CustomerHelpers.cpp" "/Users/trung/CLionProjects/cpp-renting-console-app/cmake-build-debug/CMakeFiles/cpp_renting_console_app.dir/sources/CustomerHelpers.cpp.o"
+  "/Users/trung/CLionProjects/cpp-renting-console-app/sources/CustomerRepository.cpp" "/Users/trung/CLionProjects/cpp-renting-console-app/cmake-build-debug/CMakeFiles/cpp_renting_console_app.dir/sources/CustomerRepository.cpp.o"
+  "/Users/trung/CLionProjects/cpp-renting-console-app/sources/Item.cpp" "/Users/trung/CLionProjects/cpp-renting-console-app/cmake-build-debug/CMakeFiles/cpp_renting_console_app.dir/sources/Item.cpp.o"
+  "/Users/trung/CLionProjects/cpp-renting-console-app/sources/ItemHelpers.cpp" "/Users/trung/CLionProjects/cpp-renting-console-app/cmake-build-debug/CMakeFiles/cpp_renting_console_app.dir/sources/ItemHelpers.cpp.o"
+  "/Users/trung/CLionProjects/cpp-renting-console-app/sources/ItemRepository.cpp" "/Users/trung/CLionProjects/cpp-renting-console-app/cmake-build-debug/CMakeFiles/cpp_renting_console_app.dir/sources/ItemRepository.cpp.o"
+  "/Users/trung/CLionProjects/cpp-renting-console-app/sources/Menu.cpp" "/Users/trung/CLionProjects/cpp-renting-console-app/cmake-build-debug/CMakeFiles/cpp_renting_console_app.dir/sources/Menu.cpp.o"
+  "/Users/trung/CLionProjects/cpp-renting-console-app/sources/ServiceBuilder.cpp" "/Users/trung/CLionProjects/cpp-renting-console-app/cmake-build-debug/CMakeFiles/cpp_renting_console_app.dir/sources/ServiceBuilder.cpp.o"
   )
-set(CMAKE_CXX_COMPILER_ID "GNU")
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
--- cmake-build-debug/CMakeFiles/cpp_renting_console_app.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/cpp_renting_console_app.dir/cmake_clean.cmake
@@ -1,17 +1,15 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/cpp_renting_console_app.dir/main.cpp.obj"
-  "CMakeFiles/cpp_renting_console_app.dir/sources/Customer.cpp.obj"
-  "CMakeFiles/cpp_renting_console_app.dir/sources/CustomerHelpers.cpp.obj"
-  "CMakeFiles/cpp_renting_console_app.dir/sources/CustomerRepository.cpp.obj"
-  "CMakeFiles/cpp_renting_console_app.dir/sources/Item.cpp.obj"
-  "CMakeFiles/cpp_renting_console_app.dir/sources/ItemHelpers.cpp.obj"
-  "CMakeFiles/cpp_renting_console_app.dir/sources/ItemRepository.cpp.obj"
-  "CMakeFiles/cpp_renting_console_app.dir/sources/Menu.cpp.obj"
-  "CMakeFiles/cpp_renting_console_app.dir/sources/ServiceBuilder.cpp.obj"
-  "cpp_renting_console_app.exe"
-  "cpp_renting_console_app.exe.manifest"
+  "CMakeFiles/cpp_renting_console_app.dir/main.cpp.o"
+  "CMakeFiles/cpp_renting_console_app.dir/sources/Customer.cpp.o"
+  "CMakeFiles/cpp_renting_console_app.dir/sources/CustomerHelpers.cpp.o"
+  "CMakeFiles/cpp_renting_console_app.dir/sources/CustomerRepository.cpp.o"
+  "CMakeFiles/cpp_renting_console_app.dir/sources/Item.cpp.o"
+  "CMakeFiles/cpp_renting_console_app.dir/sources/ItemHelpers.cpp.o"
+  "CMakeFiles/cpp_renting_console_app.dir/sources/ItemRepository.cpp.o"
+  "CMakeFiles/cpp_renting_console_app.dir/sources/Menu.cpp.o"
+  "CMakeFiles/cpp_renting_console_app.dir/sources/ServiceBuilder.cpp.o"
+  "cpp_renting_console_app"
   "cpp_renting_console_app.pdb"
-  "libcpp_renting_console_app.dll.a"
 )
 
 # Per-language clean rules from dependency scanning.
