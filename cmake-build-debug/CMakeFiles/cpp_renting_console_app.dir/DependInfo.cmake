e6c08cc77b4a899c3c0a088e8b33ac0f78eb9a84
trungduong0103 <trungduong0103@gmail.com>
2021-05-17 20:24:29 +0700

    added untracked files

---
+++ cmake-build-debug/CMakeFiles/cpp_renting_console_app.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/trung/CLionProjects/cpp-renting-console-app/main.cpp" "/Users/trung/CLionProjects/cpp-renting-console-app/cmake-build-debug/CMakeFiles/cpp_renting_console_app.dir/main.cpp.o"
+  "/Users/trung/CLionProjects/cpp-renting-console-app/sources/Customer.cpp" "/Users/trung/CLionProjects/cpp-renting-console-app/cmake-build-debug/CMakeFiles/cpp_renting_console_app.dir/sources/Customer.cpp.o"
+  "/Users/trung/CLionProjects/cpp-renting-console-app/sources/CustomerRepository.cpp" "/Users/trung/CLionProjects/cpp-renting-console-app/cmake-build-debug/CMakeFiles/cpp_renting_console_app.dir/sources/CustomerRepository.cpp.o"
+  "/Users/trung/CLionProjects/cpp-renting-console-app/sources/Item.cpp" "/Users/trung/CLionProjects/cpp-renting-console-app/cmake-build-debug/CMakeFiles/cpp_renting_console_app.dir/sources/Item.cpp.o"
+  "/Users/trung/CLionProjects/cpp-renting-console-app/sources/ItemHelpers.cpp" "/Users/trung/CLionProjects/cpp-renting-console-app/cmake-build-debug/CMakeFiles/cpp_renting_console_app.dir/sources/ItemHelpers.cpp.o"
+  "/Users/trung/CLionProjects/cpp-renting-console-app/sources/ItemRepository.cpp" "/Users/trung/CLionProjects/cpp-renting-console-app/cmake-build-debug/CMakeFiles/cpp_renting_console_app.dir/sources/ItemRepository.cpp.o"
+  "/Users/trung/CLionProjects/cpp-renting-console-app/sources/Menu.cpp" "/Users/trung/CLionProjects/cpp-renting-console-app/cmake-build-debug/CMakeFiles/cpp_renting_console_app.dir/sources/Menu.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/cpp_renting_console_app.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_renting_console_app.dir/main.cpp.o"
+  "CMakeFiles/cpp_renting_console_app.dir/sources/Customer.cpp.o"
+  "CMakeFiles/cpp_renting_console_app.dir/sources/CustomerRepository.cpp.o"
+  "CMakeFiles/cpp_renting_console_app.dir/sources/Item.cpp.o"
+  "CMakeFiles/cpp_renting_console_app.dir/sources/ItemHelpers.cpp.o"
+  "CMakeFiles/cpp_renting_console_app.dir/sources/ItemRepository.cpp.o"
+  "CMakeFiles/cpp_renting_console_app.dir/sources/Menu.cpp.o"
+  "cpp_renting_console_app"
+  "cpp_renting_console_app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_renting_console_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
